01529e64fd48d3e133f36e0a65015481e760d910
MeshachB <MeshachCBrown@gmail.com>
2025-10-05 20:40:15 -0500

    Ex9 join foods into 'allFoods'

--- app.js
+++ app.js
@@ -122,6 +122,23 @@ console.log(soyIdx);
 
 
 
+/*
+Exercise 9: Joining elements
+
+1) Use the `join()` method to concatenate the strings in the `foods` array, 
+   separated by ' -> '. 
+
+2) Assign the result to a variable called `allFoods`. 
+
+Note: The final result should log as:
+'taco -> sushi -> cupcake -> tofu -> cheeseburger'
+
+Complete Exercise 9 in the space below:
+*/
+const allFoods = foods.join(' -> ');
+
+
+console.log('Exercise 9 result:', allFoods);
 
 
 
